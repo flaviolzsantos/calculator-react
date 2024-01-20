3222badf62d8b90089b67ca7f727e8e9a22ff1e7
flaviolzsantos <flaviolzsantos@gmail.com>
2024-01-20 17:02:26 -0300

    first commit

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import './Calculador.css'
+
+class Calculador extends Component {
+    render(){
+        return (
+            <div className="calculator">
+            </div>
+        )
+    }
+}
+
+export default Calculador
